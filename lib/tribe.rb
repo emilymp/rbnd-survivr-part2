336dd9b31b0515028aa9c2d8768ddc45723c1e80
Emily Peri <emilyperi2@gmail.com>
2016-02-28 14:05:30 -0800

    tribe.rb passes tests

--- lib/tribe.rb
+++ lib/tribe.rb
@@ -1,2 +1,21 @@
 class Tribe
+  attr_reader :name, :members
+  
+  def initialize(op={})
+    @name = op[:name]
+    @members = op[:members]
+    puts "New Tribe #{@name}.capitalize, let the odds be ever in your favor!"
+  end
+
+  def to_s
+    @name
+  end
+
+  def tribal_council(op={})
+    immune = op[:immune]
+    members = @members.delete_if { |member| member == immune }
+    members.sample
+  end
+    
+
 end
